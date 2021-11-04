b3cc17f94029bd17cb54a69dae69a77821e96f79
Nikita <kotovnikita00744@gmail.com>
2021-11-04 20:07:11 +0700

    Initial commit

---
+++ src/main/java/com/axproject/axartemis/AxArtemisApplication.java
@@ -0,0 +1,13 @@
+package com.axproject.axartemis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxArtemisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxArtemisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axproject/axartemis/AxArtemisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axproject.axartemis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxArtemisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
